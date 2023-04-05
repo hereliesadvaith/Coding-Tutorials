c2cb917ff265b233ee07650929336e3d9a74bd30
hereliesadvaith <hereliesadvaith@gmail.com>
2023-04-05 17:52:37 +0530

    pgsqlFunctions2

--- 03_database/01_postgreSQL/10_pgsqlFunctions.sql
+++ 03_database/01_postgreSQL/10_pgsqlFunctions.sql
@@ -75,6 +75,118 @@ END;
 $body$
 LANGUAGE plpgsql
 
+CREATE OR REPLACE FUNCTION fn_get_sales_people()
+RETURNS SETOF sales_person AS
+$body$
+BEGIN
+    RETURN QUERY
+    SELECT *
+    FROM sales_person;
+END;
+$body$
+LANGUAGE plpgsql
+
+-- return specific data from a query using multiple tables
+CREATE OR REPLACE FUNCTION fn_get_product_price()
+RETURNS TABLE (
+    name varchar,
+    supplier varchar,
+    price numeric
+) AS
+$body$
+BEGIN
+    RETURN QUERY
+    SELECT product.name, product.supplier, item.price
+    FROM item
+    NATURAL JOIN product
+    ORDER BY item.price DESC;
+END
+$body$
+LANGUAGE plpgsql
+
+CREATE OR REPLACE FUNCTION fn_check_month_orders(the_month int)
+RETURNS varchar AS
+$body$
+DECLARE
+    total_orders int;
+BEGIN
+    SELECT COUNT(purchase_order_number)
+    INTO total_orders
+    FROM sales_order
+    WHERE EXTRACT(MONTH FROM time_order_taken) = the_month;
+
+    IF total_orders > 1 THEN
+        RETURN CONCAT(total_orders, 'Orders : Doing good');
+    ELSEIF total_orders < 1 THEN
+        RETURN CONCAT(total_orders, 'Orders : Doing Bad');
+    ELSE
+        RETURN CONCAT(total_orders, 'Orders : On target');
+    END IF;
+END
+$body$
+LANGUAGE plpgsql
+
+-- similar function using a case statement
+CREATE OR REPLACE FUNCTION fn_check_month_orders(the_month int)
+RETURNS varchar AS
+$body$
+DECLARE
+    total_orders int;
+BEGIN
+    SELECT COUNT(purchase_order_number)
+    INTO total_orders
+    FROM sales_order
+    WHERE EXTRACT(MONTH FROM time_order_taken) = the_month;
+
+    CASE
+        WHEN total_orders < 1 THEN
+            RETURN CONCAT(total_orders, 'Orders : Terrible');
+        WHEN total_orders > 1 THEN
+            RETURN CONCAT(total_orders, 'Orders : Good');
+        ELSE 
+            RETURN CONCAT(total_orders, 'Orders : On Target');
+    END CASE;
+END
+$body$
+LANGUAGE plpgsql
+
+/*
+LOOP
+    statements
+    EXIT WHEN condition ia TRUE;
+END LOOP
+*/
+
+CREATE OR REPLACE FUNCTION fn_loop_test(max int)
+RETURNS int AS
+$body$
+DECLARE
+    j int DEFAULT 1;
+    tot_sum int DEFAULT 0;
+BEGIN
+    LOOP
+        tot_sum := tot_sum + j;
+        j := j + 1;
+        EXIT WHEN j > max;
+    END LOOP;
+    RETURN tot_sum;
+END
+$body$
+LANGUAGE plpgsql
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
