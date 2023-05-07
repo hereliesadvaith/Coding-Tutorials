e4a1b8e0c05a531be66be86306a388102eb68f3f
hereliesadvaith <hereliesadvaith@gmail.com>
2023-05-07 22:35:54 +0530

    descriptionSpider

--- 01_python/05_libraries/01_scrapy/01/bookscraper/spiders/descriptionspider.py
+++ 01_python/05_libraries/01_scrapy/01/bookscraper/spiders/descriptionspider.py
@@ -1,4 +1,4 @@
-# crawl through pages to get the name, price of all the books
+# crawl through single book pages to get the description of books
 import scrapy
 
 
@@ -10,15 +10,8 @@ class BookspiderSpider(scrapy.Spider):
     def parse(self, response):
         # get books from response html
         books = response.css("article.product_pod")
-        # get single book
         for book in books:
-            yield {
-                "name": book.css("h3 a::text").get(),
-                "price": book.css("div p.price_color::text").get(),
-                "url": book.css("h3 a").attrib["href"],
-            }
-        next_page = response.css("li.next a ::attr(href)").get()
-        if next_page is not None:
+            next_page = response.css("li.next a ::attr(href)").get()
             if "catalogue" in next_page:
                 next_page_url = "https://books.toscrape.com/" + next_page
             else:
