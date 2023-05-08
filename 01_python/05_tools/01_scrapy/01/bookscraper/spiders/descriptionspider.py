4d361a538fe9d224c36fa7eb3dca9af82acb77f6
hereliesadvaith <hereliesadvaith@gmail.com>
2023-05-08 22:59:23 +0530

    descriptionSpider

--- 01_python/05_tools/01_scrapy/01/bookscraper/spiders/descriptionspider.py
+++ 01_python/05_tools/01_scrapy/01/bookscraper/spiders/descriptionspider.py
@@ -2,8 +2,8 @@
 import scrapy
 
 
-class BookspiderSpider(scrapy.Spider):
-    name = "bookspider"
+class DescriptionSpider(scrapy.Spider):
+    name = "descriptionspider"
     allowed_domains = ["books.toscrape.com"]
     start_urls = ["http://books.toscrape.com/"]
 
@@ -11,10 +11,21 @@ class BookspiderSpider(scrapy.Spider):
         # get books from response html
         books = response.css("article.product_pod")
         for book in books:
-            next_page = response.css("li.next a ::attr(href)").get()
-            if "catalogue" in next_page:
-                next_page_url = "https://books.toscrape.com/" + next_page
-            else:
-                next_page_url = "https://books.toscrape.com/catalogue/" + next_page
-            # recursion
-            yield response.follow(next_page_url, callback=self.parse)
+            description_url = book.css("h3 a ::attr(href)").get()
+            yield response.follow(description_url, callback=self.descrptionParse)
+        next_page = response.css("li.next a ::attr(href)").get()
+        if "catalogue" in next_page:
+            next_page_url = "https://books.toscrape.com/" + next_page
+        else:
+            next_page_url = "https://books.toscrape.com/catalogue/" + next_page
+        # recursion
+        yield response.follow(next_page_url, callback=self.parse)
+
+    def descrptionParse(self, response):
+        yield {
+            "name": response.css("h1 ::text").get(),
+            "description": response.css(
+                # if the pargraph is directly under the divsion or use p:nth-of-type(2)
+                "article.product_page > p:first-of-type::text"
+            ).get(),
+        }
