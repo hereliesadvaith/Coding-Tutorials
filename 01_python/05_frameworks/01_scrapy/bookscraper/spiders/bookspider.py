d917a249ee938de55f8c4b99daf19d1407e2809a
Advaith <hereliesadvaith@gmail.com>
2023-11-08 19:37:08 +0530

    [IMP] python/framework/scrapy: scrape next page

--- 01_python/05_frameworks/01_scrapy/bookscraper/spiders/bookspider.py
+++ 01_python/05_frameworks/01_scrapy/bookscraper/spiders/bookspider.py
@@ -15,3 +15,12 @@ class BookspiderSpider(scrapy.Spider):
                 "price": book.css(".product_price .price_color::text").get(),
                 "url": book.css("h3 a").attrib["href"]
             }
+        # to scrape next_page
+        next_page = response.css("li.next a::attr(href)").get()
+        if next_page:
+            if "catalogue" in next_page:
+                next_page_url = "https://books.toscrape.com/" + next_page
+                yield response.follow(next_page_url, callback=self.parse)
+            else:
+                next_page_url = "https://books.toscrape.com/catalogue/" + next_page
+                yield response.follow(next_page_url, callback=self.parse)
