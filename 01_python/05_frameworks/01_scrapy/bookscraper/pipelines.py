53de7a5145a6cd022c630482eb00d199e3cb7217
Advaith <hereliesadvaith@gmail.com>
2023-11-21 23:59:38 +0530

    [UPD] python/framework/scrapy: pipelines

--- 01_python/05_frameworks/01_scrapy/bookscraper/pipelines.py
+++ 01_python/05_frameworks/01_scrapy/bookscraper/pipelines.py
@@ -10,4 +10,21 @@ from itemadapter import ItemAdapter
 
 class BookscraperPipeline:
     def process_item(self, item, spider):
+        """
+        To manipulate data, to temporarly store data
+        """
+        adapter = ItemAdapter(item)
+
+        # strip all whitespaces from string
+        field_names = adapter.field_names()
+        for field_name in field_names:
+            if field_name != "name":
+                adapter[field_name] = adapter[field_name].strip()
+        # lowercase category
+        adapter["category"] = adapter["category"].lower()
+        # float price
+        adapter["price"] = float(adapter["price"])
+        # int availability
+        adapter["availability"] = int(adapter["availability"])
+
         return item
