8dc2b1c7f2ce8cde772e0bbd01a1f85752d411f4
Advaith B G <hereliesadvaith@gmail.com>
2024-09-24 21:38:15 +0530

    [ADD] python/framework/django: rest framework

--- 01_python/02_frameworks/02_django/api/views.py
+++ 01_python/02_frameworks/02_django/api/views.py
@@ -1,12 +1,22 @@
 # -*- coding: utf-8 -*-
-from django.http import JsonResponse
 from .models.product import Product
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
 
+@api_view(['GET'])
 def api_home(request):
     """
     Return all the available endpoints.
     """
-    return JsonResponse({
-        'message': 'Welcome'
-    })
+    routes = {
+        'endpoints': [
+            {
+                'endpoint': '/api/token/',
+                'method': 'POST',
+                'description': 'Obtain a new token by providing username and password.'
+            },
+        ],
+        'status': 'operational',
+    }
+    return Response(routes)
