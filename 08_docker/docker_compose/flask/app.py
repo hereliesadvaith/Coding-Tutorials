944819473af60c5fce9036200b48cf7666c80130
Advaith B G <hereliesadvaith@gmail.com>
2024-10-21 14:00:01 +0530

    [UPD] docker: docker compose flask

---
+++ 08_docker/docker_compose/flask/app.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/about', methods=['GET'])
+def about():
+    version = '0.1.0'
+    return {
+        'version': version
+    }, 200
