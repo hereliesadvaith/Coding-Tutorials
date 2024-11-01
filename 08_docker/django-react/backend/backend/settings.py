f024d1bafa766eb52788f9ebb70d441c36c77a9f
Advaith B G <hereliesadvaith@gmail.com>
2024-11-01 19:32:40 +0530

    [ADD] docker/django_react: dockerize nginx

--- 08_docker/django-react/backend/backend/settings.py
+++ 08_docker/django-react/backend/backend/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/5.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-x9$@p^^!1@kkdh3$#*7=!rnj+v2mco6vhwtgh#v^b7kl_fba%g
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -115,9 +115,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CSRF_TRUSTED_ORIGINS = ['http://localhost:8080']
--- 08_docker/django-react/backend/entrypoint.sh
+++ 08_docker/django-react/backend/entrypoint.sh
@@ -2,5 +2,6 @@
 
 python manage.py makemigrations
 python manage.py migrate --no-input
+python manage.py collectstatic --no-input
 
 gunicorn backend.wsgi:application --bind 0.0.0.0:8000
